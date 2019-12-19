40eaab886a8203dde12048a364a09ae1b6a1515e
kun19940318 <252108115@qq.com>
2019-12-19 16:52:42 +0800

    2019-12-19

---
+++ Assets/Scripts/NodeClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NodeClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ShowChild);
+    }
+    
+    private void ShowChild()
+    {
+        transform.Find("RedDot").gameObject.SetActive(false);
+        foreach (Transform child in transform.parent)
+        {
+            if (child.CompareTag("Node"))
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIRoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIRoot : MonoBehaviour
+{
+    public Transform RedDot1;
+    public Transform RedDot2;
+    public Transform RedDot3;
+    public static UITree Tree;
+
+    private void Start()
+    {
+        Tree = new UITree(transform);
+        Tree.SetRedDot(RedDot1);
+        Tree.SetRedDot(RedDot2);
+        Tree.SetRedDot(RedDot3);
+        
+    }
+}
+
+public class UITree
+{
+    public Transform Root;
+
+    public UITree(Transform root)
+    {
+        Root = root;
+    }
+    
+    public void SetRedDot(Transform node)
+    {
+        if (node.parent == null) return;
+        Transform redDot = node.Find("Button/RedDot");
+        if (redDot) redDot.gameObject.SetActive(true);
+        SetRedDot(node.parent);
+    }
+}
+
